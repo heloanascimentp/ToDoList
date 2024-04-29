608d50475d7f2edbba9f598c64ad664057aea678
Heloa Nascimento <nascimentoheloa38@gmail.com>
2024-04-29 08:49:00 -0300

    Criei a base do index, e comecei o CSS

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gerenciador de Tarefas </title>
+</head>
+<body>
+  <div class="container">
+    <div class="header">  
+        <h1>Gerenciador de Tarefas</h1>
+    </div>
+    <div class="form">
+        <form action="" method="get">
+            <label for="task_name"></label>
+            <input type="text" name="task_name" placeholder="Nome da Tarefa">
+            <button type="submit">cadastrar</button>
+        </form>
+    </div>
+    <div class="separador">
+    </div>
+    <div class="list-task">
+        <ul>
+            <li>Tarefa 1</li>
+            <li>Tarefa 2</li>
+            <li>Tarefa 3</li>
+        </ul>
+    </div>
+    <div class="footer">
+        <p>Desenvolvido por @Heloa</p>
+    </div>
+  </div>
+</body>
+</html>
